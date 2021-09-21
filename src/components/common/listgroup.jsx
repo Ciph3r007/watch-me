92a7c5138b68a0314ce461e23b943c2131af42d9
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-09-21 11:49:05 +0600

    Refactor codes to MoviesTable component

--- src/components/common/listgroup.jsx
+++ src/components/common/listgroup.jsx
@@ -2,19 +2,24 @@ import React from "react";
 
 const ListGroup = ({ items, id, name, currentItem, onItemSelect }) => {
   return (
-    <ul className="list-group">
-      {items.map((item) => (
-        <li
-          key={item[id]}
-          onClick={() => onItemSelect(item)}
-          className={
-            item === currentItem ? "list-group-item active" : "list-group-item"
-          }
-        >
-          {item[name]}
-        </li>
-      ))}
-    </ul>
+    <>
+      <p className="fs-4 mt-4 ms-3">Filters</p>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            key={item[id]}
+            onClick={() => onItemSelect(item)}
+            className={
+              item === currentItem
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+          >
+            {item[name]}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { getGenres } from "../services/fakeGenreService";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./common/like";
 import ListGroup from "./common/listgroup";
 import Pagination from "./common/pagination";
+import MoviesTable from "./moviesTable";
 import paginate from "./utils/paginate";
 
 function Movies() {
@@ -16,7 +16,6 @@ function Movies() {
   useEffect(() => {
     setMovies(getMovies());
     setGenres([{ _id: 0, name: "All movies" }, ...getGenres()]);
-    setCurrentGenre(genres[0]);
   }, []);
 
   const handleLike = (movie) => {
@@ -60,39 +59,11 @@ function Movies() {
       </div>
       <div className="col">
         <p>There are {filteredMovies.length} movies in the database</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {moviesOnPage.map((movie) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like liked={movie.liked} onClick={() => handleLike(movie)} />
-                </td>
-                <td>
-                  <button
-                    onClick={() => handleDelete(movie)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <MoviesTable
+          movies={moviesOnPage}
+          onLike={handleLike}
+          onDelete={handleDelete}
+        />
         <Pagination
           itemsCount={filteredMovies.length}
           pageSize={pageSize}
